2611ef25a962849b38454ed31ad5c09bf82078cc
Michele Congiu <micongiu@e3r9p5.42roma.it>
2025-03-25 12:03:36 +0100

    norma

--- src/ft_init.c
+++ src/ft_init.c
@@ -2,7 +2,13 @@
 
 // Funzione per mettere un pixel nel buffer dell'immagine
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
+	char	*dst;
+
+	dst = data->img_addr + (y * data->line_length
+			+ x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 	char	*dst;
 
 	dst = data->img_addr + (y * data->line_length
@@ -11,7 +17,20 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 }
 
 void	ft_init_mlx_2(t_data *data)
+void	ft_init_mlx_2(t_data *data)
 {
+	if (!data->texture_north.img || !data->texture_south.img
+		|| !data->texture_east.img || !data->texture_west.img)
+		ft_error("Error loading XPM images");
+	data->texture_sky.img = mlx_xpm_file_to_image(data->mlx,
+			"./test.xpm/sky.xpm", &data->texture_sky.width,
+			&data->texture_sky.height);
+	data->texture_grass.img = mlx_xpm_file_to_image(data->mlx,
+			"./test.xpm/grass.xpm", &data->texture_grass.width,
+			&data->texture_grass.height);
+}
+
+void	ft_init_mlx(t_data *data)
 	if (!data->texture_north.img || !data->texture_south.img
 		|| !data->texture_east.img || !data->texture_west.img)
 		ft_error("Error loading XPM images");
@@ -58,6 +77,26 @@ void	ft_init_mlx(t_data *data)
 		i++;
 	}
 	return 0;
+	data->mlx = mlx_init();
+	data->win = mlx_new_window(data->mlx, data->map_width * TILE_SIZE,
+			data->map_height * TILE_SIZE, "Cub3D Map");
+	data->img_buffer = mlx_new_image(data->mlx, data->map_width * TILE_SIZE,
+			data->map_height * TILE_SIZE);
+	data->img_addr = mlx_get_data_addr(data->img_buffer, &data->bits_per_pixel,
+			&data->line_length, &data->endian);
+	data->texture_north.img = mlx_xpm_file_to_image(data->mlx,
+			"./test.xpm/b_w_n.xpm", &data->texture_north.width,
+			&data->texture_north.height);
+	data->texture_south.img = mlx_xpm_file_to_image(data->mlx,
+			"./test.xpm/b_w_s.xpm", &data->texture_south.width,
+			&data->texture_south.height);
+	data->texture_east.img = mlx_xpm_file_to_image(data->mlx,
+			"./test.xpm/b_w_o.xpm", &data->texture_east.width,
+			&data->texture_east.height);
+	data->texture_west.img = mlx_xpm_file_to_image(data->mlx,
+			"./test.xpm/b_w_w.xpm", &data->texture_west.width,
+			&data->texture_west.height);
+	ft_init_mlx_2(data);
 }
 void find_player_orientation(t_data *data)
 {
@@ -107,10 +146,13 @@ void find_player_orientation(t_data *data)
 }
 
 void	ft_init_data(t_data *data, char *argv)
+
+void	ft_init_data(t_data *data, char *argv)
 {
 	data->map = open_file(argv);
 
 	calculate_map_dimensions(data);
+	if (parser_map(data) == 1)
 	if (parser_map(data) == 1)
 		ft_error("Error in the parser\n");
 	find_player_orientation(data);
@@ -126,6 +168,7 @@ void	ft_init_data(t_data *data, char *argv)
 		ft_error("Error loading XPM images");
 	data->texture_sky.img = mlx_xpm_file_to_image(data->mlx, "./test.xpm/sky.xpm", &data->texture_sky.width, &data->texture_sky.height);
 	data->texture_grass.img = mlx_xpm_file_to_image(data->mlx, "./test.xpm/grass.xpm", &data->texture_grass.width, &data->texture_grass.height);
+	ft_init_mlx(data);
 	if (!data->texture_sky.img || !data->texture_grass.img)
 		ft_error("Error loading sky or grass XPM images");
 	convert_texture_data(data, &data->texture_sky);
